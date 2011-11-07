fece0d0cfec308918fdc9c88587102dd2f465646
vanevery <vanevery@walking-productions.com>
2011-11-07 16:01:24 -0500

    Starting on region creation

--- src/org/witness/sscvideoproto/VideoEditor.java
+++ src/org/witness/sscvideoproto/VideoEditor.java
@@ -59,7 +59,6 @@ public class VideoEditor extends Activity implements
 	ImageButton outPointImageButton;
 	
 	private Vector obscureRegions = new Vector();
-
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -71,6 +70,7 @@ public class VideoEditor extends Activity implements
 		originalVideoUri = getIntent().getData();
 
 		surfaceView = (SurfaceView) this.findViewById(R.id.SurfaceView);
+		surfaceView.setOnTouchListener(this);
 		surfaceHolder = surfaceView.getHolder();
 
 		surfaceHolder.addCallback(this);
@@ -309,8 +309,245 @@ public class VideoEditor extends Activity implements
 		   mHandler.postDelayed(this, 1000);
 	   }
 	};		
+	
+	/* TOUCH EVENTS FROM ImageEditor.java 
+	ImageRegion currRegion = null;
+	
+	@Override
+	public boolean onTouch(View v, MotionEvent event) 
+	{
+		if (currRegion != null && (mode == DRAG || currRegion.getBounds().contains(event.getX(), event.getY())))		
+			return onTouchRegion(v, event, currRegion);	
+		else
+			return onTouchImage(v,event);
+	}
+	
+	public ImageRegion findRegion (MotionEvent event)
+	{
+		ImageRegion result = null;
+		Matrix iMatrix = new Matrix();
+		matrix.invert(iMatrix);
+
+		float[] points = {event.getX(), event.getY()};        	
+    	iMatrix.mapPoints(points);
+    	
+		for (ImageRegion region : imageRegions)
+		{
+
+			if (region.getBounds().contains(points[0],points[1]))
+			{
+				result = region;
+				
+				break;
+			}
+			
+		}
+	
+		
+		return result;
+	}
+	
+	public boolean onTouchRegion (View v, MotionEvent event, ImageRegion iRegion)
+	{
+		boolean handled = false;
+		
+		currRegion.setMatrix(matrix);
+		
+		switch (event.getAction() & MotionEvent.ACTION_MASK) {
+			case MotionEvent.ACTION_DOWN:
+				clearImageRegionsEditMode();
+				currRegion.setSelected(true);	
+				
+				currRegion.setCornerMode(event.getX(),event.getY());
+				
+				mode = DRAG;
+				handled = iRegion.onTouch(v, event);
+
+			break;
+			
+			case MotionEvent.ACTION_UP:
+				mode = NONE;
+				handled = iRegion.onTouch(v, event);
+				currRegion.setSelected(false);
+				//if (handled)
+					//currRegion = null;
+			
+			break;
+			
+			default:
+				mode = DRAG;
+				handled = iRegion.onTouch(v, event);
+			
+		}
+		
+		return handled;
+		
+		
+	}
+	
+	public boolean onTouchImage(View v, MotionEvent event) 
+	{
+		boolean handled = false;
+		
+		switch (event.getAction() & MotionEvent.ACTION_MASK) {
+			case MotionEvent.ACTION_DOWN:
+				// Single Finger down
+				mode = TAP;				
+				ImageRegion newRegion = findRegion(event);
+				
+				if (newRegion != null)
+				{
+					currRegion = newRegion;
+					return onTouchRegion(v,  event, currRegion);
+				}
+				else if (currRegion == null)
+				{
+					
+					// 	Save the Start point. 
+					startPoint.set(event.getX(), event.getY());
+				}
+				else
+				{
+					currRegion.setSelected(false);
+					currRegion = null;
+
+				}
+				
+				
+				break;
+				
+			case MotionEvent.ACTION_POINTER_DOWN:
+				// Two Fingers down
+
+				// Don't do realtime preview while touching screen
+				//doRealtimePreview = false;
+				//updateDisplayImage();
+
+				// Get the spacing of the fingers, 2 fingers
+				float sx = event.getX(0) - event.getX(1);
+				float sy = event.getY(0) - event.getY(1);
+				startFingerSpacing = (float) Math.sqrt(sx * sx + sy * sy);
+
+				//Log.d(ObscuraApp.TAG, "Start Finger Spacing=" + startFingerSpacing);
+				
+				// Get the midpoint
+				float xsum = event.getX(0) + event.getX(1);
+				float ysum = event.getY(0) + event.getY(1);
+				startFingerSpacingMidPoint.set(xsum / 2, ysum / 2);
+				
+				mode = ZOOM;
+				//Log.d(ObscuraApp.TAG, "mode=ZOOM");
+				
+				clearImageRegionsEditMode();
+				
+				break;
+				
+			case MotionEvent.ACTION_UP:
+				// Single Finger Up
+				
+			    // Re-enable realtime preview
+				doRealtimePreview = true;
+				updateDisplayImage();
+				
+				//debug(ObscuraApp.TAG,"mode=NONE");
+
+				break;
+				
+			case MotionEvent.ACTION_POINTER_UP:
+				// Multiple Finger Up
+				
+			    // Re-enable realtime preview
+				doRealtimePreview = true;
+				updateDisplayImage();
+				
+				//Log.d(ObscuraApp.TAG, "mode=NONE");
+				break;
+				
+			case MotionEvent.ACTION_MOVE:
+				
+				// Calculate distance moved
+				float distance = (float) (Math.sqrt(Math.abs(startPoint.x - event.getX()) + Math.abs(startPoint.y - event.getY())));
+				//debug(ObscuraApp.TAG,"Move Distance: " + distance);
+				//debug(ObscuraApp.TAG,"Min Distance: " + minMoveDistance);
+				
+				// If greater than minMoveDistance, it is likely a drag or zoom
+				if (distance > minMoveDistance) {
+				
+					if (mode == TAP || mode == DRAG) {
+						mode = DRAG;
+						
+						matrix.postTranslate(event.getX() - startPoint.x, event.getY() - startPoint.y);
+						imageView.setImageMatrix(matrix);
+//						// Reset the start point
+						startPoint.set(event.getX(), event.getY());
+	
+						putOnScreen();
+						//redrawRegions();
+						
+						handled = true;
+	
+					} else if (mode == ZOOM) {
+						
+						// Save the current matrix so that if zoom goes to big, we can revert
+						savedMatrix.set(matrix);
+						
+						// Get the spacing of the fingers, 2 fingers
+						float ex = event.getX(0) - event.getX(1);
+						float ey = event.getY(0) - event.getY(1);
+						endFingerSpacing = (float) Math.sqrt(ex * ex + ey * ey);
+	
+						//Log.d(ObscuraApp.TAG, "End Finger Spacing=" + endFingerSpacing);
+		
+						// If we moved far enough
+						if (endFingerSpacing > minMoveDistance) {
+							
+							// Ratio of spacing..  If it was 5 and goes to 10 the image is 2x as big
+							float scale = endFingerSpacing / startFingerSpacing;
+							// Scale from the midpoint
+							matrix.postScale(scale, scale, startFingerSpacingMidPoint.x, startFingerSpacingMidPoint.y);
+		
+							// Make sure that the matrix isn't bigger than max scale/zoom
+							float[] matrixValues = new float[9];
+							matrix.getValues(matrixValues);
+							
+							if (matrixValues[0] > MAX_SCALE) {
+								matrix.set(savedMatrix);
+							}
+							imageView.setImageMatrix(matrix);
+							
+							putOnScreen();
+							//redrawRegions();
+	
+							// Reset Start Finger Spacing
+							float esx = event.getX(0) - event.getX(1);
+							float esy = event.getY(0) - event.getY(1);
+							startFingerSpacing = (float) Math.sqrt(esx * esx + esy * esy);
+							//Log.d(ObscuraApp.TAG, "New Start Finger Spacing=" + startFingerSpacing);
+							
+							// Reset the midpoint
+							float x_sum = event.getX(0) + event.getX(1);
+							float y_sum = event.getY(0) + event.getY(1);
+							startFingerSpacingMidPoint.set(x_sum / 2, y_sum / 2);
+							
+							handled = true;
+						}
+					}
+				}
+				break;
+		}
+
+
+		return handled; // indicate event was handled
+	}
+	*/	
+
+	ObscureRegion currRegion = null;
 
 	int currentNumFingers = 0;
+	
+	public static final int NONE = 0;
+	public static final int DRAG = 1;
+	int mode = NONE;
 
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
@@ -318,6 +555,11 @@ public class VideoEditor extends Activity implements
 			Log.v(LOGTAG,"" + event.getX() + " " + event.getX()/progressBar.getWidth());
 			mediaPlayer.seekTo((int)(mediaPlayer.getDuration()*(float)(event.getX()/progressBar.getWidth())));
 			return false;
+		}
+		else if (currRegion != null && (mode == DRAG || currRegion.getBounds().contains(event.getX(), event.getY())))		
+		{
+			boolean handled = false;
+			return handled;
 		}
 		else 
 		{
