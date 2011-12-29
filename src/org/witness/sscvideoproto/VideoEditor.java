d77f4ceb33291437d4333572de30c91e192a459e
vanevery <vanevery@walking-productions.com>
2011-12-29 16:16:27 -0500

    Started using new scrubber with in and out points

--- src/org/witness/sscvideoproto/VideoEditor.java
+++ src/org/witness/sscvideoproto/VideoEditor.java
@@ -1,8 +1,19 @@
+/*
+ * 
+ * To Do:
+ * 
+ * Handles on SeekBar - two thumb scrubber will be for current region (tempRegion)
+ * RegionBarArea/RegionBar will be graphical display of region tracks, no editing, just selecting
+ * 
+ */
+
 package org.witness.sscvideoproto;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Vector;
+
+import org.witness.sscvideoproto.InOutPlayheadSeekBar.InOutPlayheadSeekBarChangeListener;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -57,7 +68,8 @@ public class VideoEditor extends Activity implements
 						OnCompletionListener, OnErrorListener, OnInfoListener,
 						OnBufferingUpdateListener, OnPreparedListener, OnSeekCompleteListener,
 						OnVideoSizeChangedListener, SurfaceHolder.Callback,
-						MediaController.MediaPlayerControl, OnTouchListener, OnClickListener {
+						MediaController.MediaPlayerControl, OnTouchListener, OnClickListener,
+						InOutPlayheadSeekBarChangeListener {
 
 	public static final String LOGTAG = "VIDEOEDITOR";
 	public static final String PACKAGENAME = "org.witness.sscvideoproto";
@@ -91,16 +103,13 @@ public class VideoEditor extends Activity implements
 	Bitmap bitmapCornerLL;
 	Bitmap bitmapCornerLR;
 	
-	ProgressBar progressBar;
+	InOutPlayheadSeekBar progressBar;
+	RegionBarArea regionBarArea;
 	
-	SeekBar seekBar;
-
 	int videoWidth = 0;
 	int videoHeight = 0;
 	
 	ImageButton playPauseButton;
-	ImageButton inPointImageButton;
-	ImageButton outPointImageButton;
 	
 	private Vector<ObscureRegion> obscureRegions = new Vector<ObscureRegion>();
 	private ObscureRegion tempRegion;
@@ -155,30 +164,26 @@ public class VideoEditor extends Activity implements
 			finish();
 		}
 		
-		progressBar = (ProgressBar) this.findViewById(R.id.ProgressBar);
+		progressBar = (InOutPlayheadSeekBar) this.findViewById(R.id.InOutPlayheadSeekBar);
 
-		//progressBar.setProgressDrawable(getResources().getDrawable(R.drawable.progressbar));
 		progressBar.setIndeterminate(false);
 		progressBar.setSecondaryProgress(0);
 		progressBar.setProgress(0);
+		progressBar.setInOutPlayheadSeekBarChangeListener(this);
+		progressBar.setThumbsInactive();
 		progressBar.setOnTouchListener(this);
+
 
 		playPauseButton = (ImageButton) this.findViewById(R.id.PlayPauseImageButton);
 		playPauseButton.setOnClickListener(this);
 		
-		/*
-		inPointImageButton = (ImageButton) this.findViewById(R.id.InPointImageButton);
-		inPointImageButton.setOnClickListener(this);
-		outPointImageButton = (ImageButton) this.findViewById(R.id.OutPointImageButton);
-		outPointImageButton.setOnClickListener(this);
-		*/
-		
+	
 		currentDisplay = getWindowManager().getDefaultDisplay();
 				
 		redactSettingsFile = new File(Environment.getExternalStorageDirectory().getPath()+"/"+PACKAGENAME+"/redact_unsort.txt");
 		ffmpeg = new FFMPEGWrapper(this.getBaseContext());
 		
-		seekBar = (SeekBar) this.findViewById(R.id.CustomSeekBar);
+		regionBarArea = (RegionBarArea) this.findViewById(R.id.RegionBarArea);
 		
 		obscuredPaint = new Paint();   
         obscuredPaint.setColor(Color.WHITE);
@@ -454,237 +459,6 @@ public class VideoEditor extends Activity implements
 		clearPaint.setXfermode(new PorterDuffXfermode(Mode.CLEAR));
 		obscuredCanvas.drawPaint(clearPaint);
 	}
-	
-	/* TOUCH EVENTS FROM ImageEditor.java 
-	ImageRegion currRegion = null;
-	
-	@Override
-	public boolean onTouch(View v, MotionEvent event) 
-	{
-		if (currRegion != null && (mode == DRAG || currRegion.getBounds().contains(event.getX(), event.getY())))		
-			return onTouchRegion(v, event, currRegion);	
-		else
-			return onTouchImage(v,event);
-	}
-	
-	public ImageRegion findRegion (MotionEvent event)
-	{
-		ImageRegion result = null;
-		Matrix iMatrix = new Matrix();
-		matrix.invert(iMatrix);
-
-		float[] points = {event.getX(), event.getY()};        	
-    	iMatrix.mapPoints(points);
-    	
-		for (ImageRegion region : imageRegions)
-		{
-
-			if (region.getBounds().contains(points[0],points[1]))
-			{
-				result = region;
-				
-				break;
-			}
-			
-		}
-	
-		
-		return result;
-	}
-	
-	public boolean onTouchRegion (View v, MotionEvent event, ImageRegion iRegion)
-	{
-		boolean handled = false;
-		
-		currRegion.setMatrix(matrix);
-		
-		switch (event.getAction() & MotionEvent.ACTION_MASK) {
-			case MotionEvent.ACTION_DOWN:
-				clearImageRegionsEditMode();
-				currRegion.setSelected(true);	
-				
-				currRegion.setCornerMode(event.getX(),event.getY());
-				
-				mode = DRAG;
-				handled = iRegion.onTouch(v, event);
-
-			break;
-			
-			case MotionEvent.ACTION_UP:
-				mode = NONE;
-				handled = iRegion.onTouch(v, event);
-				currRegion.setSelected(false);
-				//if (handled)
-					//currRegion = null;
-			
-			break;
-			
-			default:
-				mode = DRAG;
-				handled = iRegion.onTouch(v, event);
-			
-		}
-		
-		return handled;
-		
-		
-	}
-	
-	public boolean onTouchImage(View v, MotionEvent event) 
-	{
-		boolean handled = false;
-		
-		switch (event.getAction() & MotionEvent.ACTION_MASK) {
-			case MotionEvent.ACTION_DOWN:
-				// Single Finger down
-				mode = TAP;				
-				ImageRegion newRegion = findRegion(event);
-				
-				if (newRegion != null)
-				{
-					currRegion = newRegion;
-					return onTouchRegion(v,  event, currRegion);
-				}
-				else if (currRegion == null)
-				{
-					
-					// 	Save the Start point. 
-					startPoint.set(event.getX(), event.getY());
-				}
-				else
-				{
-					currRegion.setSelected(false);
-					currRegion = null;
-
-				}
-				
-				
-				break;
-				
-			case MotionEvent.ACTION_POINTER_DOWN:
-				// Two Fingers down
-
-				// Don't do realtime preview while touching screen
-				//doRealtimePreview = false;
-				//updateDisplayImage();
-
-				// Get the spacing of the fingers, 2 fingers
-				float sx = event.getX(0) - event.getX(1);
-				float sy = event.getY(0) - event.getY(1);
-				startFingerSpacing = (float) Math.sqrt(sx * sx + sy * sy);
-
-				//Log.d(ObscuraApp.TAG, "Start Finger Spacing=" + startFingerSpacing);
-				
-				// Get the midpoint
-				float xsum = event.getX(0) + event.getX(1);
-				float ysum = event.getY(0) + event.getY(1);
-				startFingerSpacingMidPoint.set(xsum / 2, ysum / 2);
-				
-				mode = ZOOM;
-				//Log.d(ObscuraApp.TAG, "mode=ZOOM");
-				
-				clearImageRegionsEditMode();
-				
-				break;
-				
-			case MotionEvent.ACTION_UP:
-				// Single Finger Up
-				
-			    // Re-enable realtime preview
-				doRealtimePreview = true;
-				updateDisplayImage();
-				
-				//debug(ObscuraApp.TAG,"mode=NONE");
-
-				break;
-				
-			case MotionEvent.ACTION_POINTER_UP:
-				// Multiple Finger Up
-				
-			    // Re-enable realtime preview
-				doRealtimePreview = true;
-				updateDisplayImage();
-				
-				//Log.d(ObscuraApp.TAG, "mode=NONE");
-				break;
-				
-			case MotionEvent.ACTION_MOVE:
-				
-				// Calculate distance moved
-				float distance = (float) (Math.sqrt(Math.abs(startPoint.x - event.getX()) + Math.abs(startPoint.y - event.getY())));
-				//debug(ObscuraApp.TAG,"Move Distance: " + distance);
-				//debug(ObscuraApp.TAG,"Min Distance: " + minMoveDistance);
-				
-				// If greater than minMoveDistance, it is likely a drag or zoom
-				if (distance > minMoveDistance) {
-				
-					if (mode == TAP || mode == DRAG) {
-						mode = DRAG;
-						
-						matrix.postTranslate(event.getX() - startPoint.x, event.getY() - startPoint.y);
-						imageView.setImageMatrix(matrix);
-//						// Reset the start point
-						startPoint.set(event.getX(), event.getY());
-	
-						putOnScreen();
-						//redrawRegions();
-						
-						handled = true;
-	
-					} else if (mode == ZOOM) {
-						
-						// Save the current matrix so that if zoom goes to big, we can revert
-						savedMatrix.set(matrix);
-						
-						// Get the spacing of the fingers, 2 fingers
-						float ex = event.getX(0) - event.getX(1);
-						float ey = event.getY(0) - event.getY(1);
-						endFingerSpacing = (float) Math.sqrt(ex * ex + ey * ey);
-	
-						//Log.d(ObscuraApp.TAG, "End Finger Spacing=" + endFingerSpacing);
-		
-						// If we moved far enough
-						if (endFingerSpacing > minMoveDistance) {
-							
-							// Ratio of spacing..  If it was 5 and goes to 10 the image is 2x as big
-							float scale = endFingerSpacing / startFingerSpacing;
-							// Scale from the midpoint
-							matrix.postScale(scale, scale, startFingerSpacingMidPoint.x, startFingerSpacingMidPoint.y);
-		
-							// Make sure that the matrix isn't bigger than max scale/zoom
-							float[] matrixValues = new float[9];
-							matrix.getValues(matrixValues);
-							
-							if (matrixValues[0] > MAX_SCALE) {
-								matrix.set(savedMatrix);
-							}
-							imageView.setImageMatrix(matrix);
-							
-							putOnScreen();
-							//redrawRegions();
-	
-							// Reset Start Finger Spacing
-							float esx = event.getX(0) - event.getX(1);
-							float esy = event.getY(0) - event.getY(1);
-							startFingerSpacing = (float) Math.sqrt(esx * esx + esy * esy);
-							//Log.d(ObscuraApp.TAG, "New Start Finger Spacing=" + startFingerSpacing);
-							
-							// Reset the midpoint
-							float x_sum = event.getX(0) + event.getX(1);
-							float y_sum = event.getY(0) + event.getY(1);
-							startFingerSpacingMidPoint.set(x_sum / 2, y_sum / 2);
-							
-							handled = true;
-						}
-					}
-				}
-				break;
-		}
-
-
-		return handled; // indicate event was handled
-	}
-	*/	
 
 	int currentNumFingers = 0;
 	int regionCornerMode = 0;
@@ -723,6 +497,7 @@ public class VideoEditor extends Activity implements
 
 		if (v == progressBar) {
 			// It's the progress bar/scrubber
+			
 			Log.v(LOGTAG,"" + event.getX() + " " + event.getX()/progressBar.getWidth());
 			Log.v(LOGTAG,"Seeking To: " + (int)(mediaPlayer.getDuration()*(float)(event.getX()/progressBar.getWidth())));
 			mediaPlayer.seekTo((int)(mediaPlayer.getDuration()*(float)(event.getX()/progressBar.getWidth())));
@@ -732,15 +507,17 @@ public class VideoEditor extends Activity implements
 				mediaPlayer.start();
 				mediaPlayer.pause();
 			}
+			handled = false; // The progress bar doesn't get it if we have true here
 		}
 		else
 		{
-			// New Region Related
+			// Region Related
 			float x = event.getX()/(float)currentDisplay.getWidth() * videoWidth;
 			float y = event.getY()/(float)currentDisplay.getHeight() * videoHeight;
 
 			switch (event.getAction() & MotionEvent.ACTION_MASK) {
 				case MotionEvent.ACTION_DOWN:
+
 					// Single Finger down
 					currentNumFingers = 1;
 					
@@ -769,6 +546,9 @@ public class VideoEditor extends Activity implements
 							// Are we on a corner?
 							regionCornerMode = getRegionCornerMode(tempRegion, x, y);
 							
+							// Show in and out points
+							progressBar.setThumbsActive((int)(tempRegion.startTime/mediaPlayer.getDuration()*100), (int)(tempRegion.endTime/mediaPlayer.getDuration()*100));
+							
 							// They are interacting with the active region
 							Log.v(LOGTAG,"Touched an existing region");
 						}
@@ -776,6 +556,10 @@ public class VideoEditor extends Activity implements
 						{
 							tempRegion = new ObscureRegion(mediaPlayer.getCurrentPosition(),mediaPlayer.getDuration(),x,y);
 							Log.v(LOGTAG,"Creating a new tempRegion");
+							
+							// Show in and out points
+							progressBar.setThumbsActive((int)(tempRegion.startTime/mediaPlayer.getDuration()*100), (int)(tempRegion.endTime/mediaPlayer.getDuration()*100));
+
 						}
 					}
 
@@ -795,14 +579,16 @@ public class VideoEditor extends Activity implements
 						tempRegion.endTime = mediaPlayer.getCurrentPosition();
 						obscureRegions.add(tempRegion);
 						
-						//RegionBar rb = new RegionBar(this);
-						SeekBarWithTwoThumb rb = new SeekBarWithTwoThumb(this);
-						LayoutParams llp = new LayoutParams(100, 50);
-						rb.setLayoutParams(llp);
-						//rb.or = tempRegion;
-						seekBar.addView(rb);
+						RegionBar rb = new RegionBar(this);
+						rb.or = tempRegion;
+						LayoutParams rbllp = new LayoutParams(100, 50);
+						rb.setLayoutParams(rbllp);
+						regionBarArea.addView(rb);
 
 						tempRegion = null;
+						
+						// Hide in and out points
+						progressBar.setThumbsInactive();
 						
 						showMenu = false;
 					}
@@ -820,12 +606,11 @@ public class VideoEditor extends Activity implements
 						tempRegion.endTime = mediaPlayer.getCurrentPosition();
 						obscureRegions.add(tempRegion);
 
-						//RegionBar rb = new RegionBar(this);
-						SeekBarWithTwoThumb rb = new SeekBarWithTwoThumb(this);
-						LayoutParams llp = new LayoutParams(100, 50);
-						rb.setLayoutParams(llp);
-						//rb.or = tempRegion;
-						seekBar.addView(rb);
+						RegionBar rb = new RegionBar(this);
+						rb.or = tempRegion;
+						LayoutParams rbllp = new LayoutParams(100, 50);
+						rb.setLayoutParams(rbllp);
+						regionBarArea.addView(rb);
 						
 						ObscureRegion lastRegion = tempRegion;
 						tempRegion = null;
@@ -847,6 +632,12 @@ public class VideoEditor extends Activity implements
 							// No Corner
 							tempRegion = new ObscureRegion(mediaPlayer.getCurrentPosition(),previousEndTime,x,y);
 						}
+						
+						if (tempRegion != null) {
+							// Show in and out points
+							progressBar.setThumbsActive((int)(tempRegion.startTime/mediaPlayer.getDuration()*100), (int)(tempRegion.endTime/mediaPlayer.getDuration()*100));
+						}
+						
 					} else if (tempRegion != null) {
 						Log.v(LOGTAG,"Moving tempRegion start time");
 						
@@ -866,6 +657,10 @@ public class VideoEditor extends Activity implements
 							// No Corner
 							tempRegion.moveRegion(x, y);
 						}
+						
+						// Show in and out points
+						progressBar.setThumbsActive((int)(tempRegion.startTime/mediaPlayer.getDuration()*100), (int)(tempRegion.endTime/mediaPlayer.getDuration()*100));
+
 					}
 					
 					handled = true;
@@ -1056,5 +851,12 @@ public class VideoEditor extends Activity implements
 	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		super.onActivityResult(requestCode, resultCode, intent);
 		showPlayShareDialog();
+	}
+
+	@Override
+	public void inOutValuesChanged(int thumbInValue, int thumbOutValue) {
+		if (tempRegion != null) {
+			//asdf
+		}
 	}	
 }
